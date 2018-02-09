5f6226e5a36593a9f8c8fffafc2d0ccc4d554c26
ik012982i9 <ik012982i9@206-15-HZ336131.H012982.NET>
2018-02-09 08:55:20 +0100

    tests

---
+++ src/Tests.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class Tests {
+
+	@Test(expected = FileNotFoundException.class)
+	public void testLector1() throws FileNotFoundException {
+
+		Serie serie = new Serie();
+		serie.setNombreFichero("kaixo");
+		serie.creaLector();
+
+	}
+
+	@Test
+	public void testLector2() {
+
+		Serie serie = new Serie();
+		serie.setNombreFichero("kaixo");
+		try {
+			serie.creaLector();
+			fail("Se esperaba FileNotfoundException");
+		} catch (FileNotFoundException e) {
+
+		}
+
+	}
+
+	
+
+}
